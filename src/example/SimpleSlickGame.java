d9ee444d54e8a84be25fea30e9be54dad4782a44
Benjamin Ejlertsen <benjaminejlertsen@hotmail.com>
2015-03-11 13:55:58 +0100

    Player rotation added. Player input added

--- src/example/SimpleSlickGame.java
+++ src/example/SimpleSlickGame.java
@@ -8,6 +8,7 @@ import org.newdawn.slick.Color;
 import org.newdawn.slick.GameContainer;
 import org.newdawn.slick.Graphics;
 import org.newdawn.slick.Image;
+import org.newdawn.slick.Input;
 import org.newdawn.slick.SlickException;
 
 //Det virker! Nu vi klar!
@@ -17,6 +18,8 @@ public class SimpleSlickGame extends BasicGame
 	
 	private Image player = null;
 	private Color white = new Color(255,255,255);
+	private float scale = 0.3f;
+	private float rotation = 0;
 	
 	public SimpleSlickGame(String gamename)
 	{
@@ -27,10 +30,21 @@ public class SimpleSlickGame extends BasicGame
 	public void init(GameContainer gc) throws SlickException {
 		
 		player = new Image("graphics/playerWhite.png");
+		player.setCenterOfRotation(player.getWidth() * scale / 2, player.getHeight() * scale / 2); //Set the origin of the player sprite
 	}
 
 	@Override
-	public void update(GameContainer gc, int i) throws SlickException {}
+	public void update(GameContainer gc, int i) throws SlickException {
+		Input input = gc.getInput(); //Init input class
+		//Set player rotation
+		if(input.isKeyDown(Input.KEY_D)){
+			rotation = 5;
+		}
+		else if (input.isKeyDown(Input.KEY_A)){
+			rotation = -5;
+		}
+		else rotation = 0;
+	}
 
 	@Override
 	public void render(GameContainer gc, Graphics g) throws SlickException
@@ -38,7 +52,8 @@ public class SimpleSlickGame extends BasicGame
 		g.drawString("Hello World!", 250, 200);
 		g.drawString("This is bad", 200,100);
 		
-		player.draw(50f,50f,0.3f);
+		player.rotate(rotation); //Rotate the player
+		player.draw(50f,50f,scale); //Draw the player
 	}
 
 	public static void main(String[] args)
