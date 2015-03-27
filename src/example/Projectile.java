34aec04a5b086b169289706c25c8d53bf2d9c37f
hanstheman83 <hey2k_bad@hushmail.com>
2015-03-27 12:14:03 +0100

    jj

---
+++ src/example/Projectile.java
@@ -0,0 +1,25 @@
+package example;
+
+//check player position and angle, 
+//one at a time, several objects, list of objects, removed from list depending on time!! 
+//graphics, position angle and render, 
+//all projectiles should check collision!! thus need not check collision on projectiles outside screen!! 
+//a projectile has a countdown timer
+
+public class Projectile {
+
+	
+	
+	public class ProjectileObj{
+	
+		
+		void destroyProjectile(){//when collided
+		
+	
+	}
+		
+		
+		
+	}
+	
+}
